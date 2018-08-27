fe6447d648279a27aa2c1d080715b9a5f65ad06e
Jor Ming Poon <jormpoon@gmail.com>
2018-08-27 19:52:29 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,30 @@
+var chocolateBars = ['snickers', 'hundredittles'];
+
+function addElementToBeginningOfArray( array, element ) {
+   var a1 = array;
+   return [element, ...a1];
+};
+
+function destructivelyAddElementToBeginningOfArray( array, element) {
+ array.unshift(element);
+ return array;
+};
+
+var addElementToEndOfArray = ( array, element) => {
+  var a1 = array;
+  return [...a1, element];
+}
+
+var destructivelyAddElementToEndOfArray = ( array, element) => {
+   array.push(element);
+   return array;
+}
+
+var accessElementInArray = (array, index) => {
+  return array[index];
+}
+
+var destructivelyRemoveElementFromBeginningOfArray = (array) => {
+  array.shift();
+  return array;
+}
